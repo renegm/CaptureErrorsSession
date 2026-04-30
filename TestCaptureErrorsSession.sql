c7f7b82a29ab8302d4a20e7c125478d5cf542e4c
rene <renegm@live.com>
2026-04-29 20:48:46 -0400

    Readme/ added create table on test

--- TestCaptureErrorsSession.sql
+++ TestCaptureErrorsSession.sql
@@ -1,23 +1,27 @@
+SET NOCOUNT ON;
+DROP TABLE IF EXISTS dbo.TestError;
+CREATE TABLE dbo.TestError (x int DEFAULT(0));
+
 DECLARE @result nvarchar(MAX), @SessionEvent sysname = 'SessionEventqqq';
 
 EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Create', @result = @result OUTPUT;
-PRINT 'Create' + @result;
+PRINT 'Create: ' + @result;
 EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Start', @result = @result OUTPUT;
-PRINT 'Start' + @result;
+PRINT 'Start: ' + @result;
 BEGIN TRY
-  
     ALTER TABLE dbo.TestError ALTER COLUMN x bigint;
-    PRINT ''
 END TRY
 BEGIN CATCH
-    PRINT '';
+    PRINT '-------ERROR_MESSAGE----------';
+    PRINT ERROR_MESSAGE();
+    PRINT '-------ERROR_MESSAGE----------';
+    EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Get', @result = @result OUTPUT;
+    PRINT 'Get: ' + @result;
 END CATCH;
-
-EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Get', @result = @result OUTPUT;
-PRINT 'Get' + @result;
 EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Stop', @result = @result OUTPUT;
-PRINT 'Stop' + @result;
+PRINT 'Stop: ' + @result;
 EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Stop', @result = @result OUTPUT;
-PRINT 'Stop' + @result;
+PRINT 'Doble Stop: ' + @result;
 EXEC Tools.CaptureErrorsSession @SessionEvent = @SessionEvent, @Action = 'Drop', @result = @result OUTPUT;
-PRINT 'Drop' + @result;
+PRINT 'Drop: ' + @result;
+DROP TABLE IF EXISTS dbo.TestError;
